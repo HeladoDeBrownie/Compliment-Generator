00a80586e16eaafd931aa2dec69ae6cb68e2541f
helado de brownie <helado.de.brownie@gmail.com>
2020-12-23 20:25:46 -0500

    start fleshing out grammar

--- grammar.js
+++ grammar.js
@@ -1,3 +1,49 @@
 var grammar = tracery.createGrammar({
-    origin: "hee hoo",
+    origin: "#compliment.capitalize#! #emoji#",
+
+    compliment: [
+        "I appreciate how hard you work",
+        "you have a nice #niceQuality#",
+    ],
+
+    niceQuality: [
+        "laugh",
+        "smile",
+    ],
+
+    emoji: [
+        "#emojiEyes##emojiMouth#",
+        "#emojiUncommon#",
+    ],
+
+    emojiEyes: [
+        ":",
+        ":",
+        ":",
+        ":",
+        "8",
+    ],
+
+    emojiMouth: [
+        ")",
+        "#emojiMouthUncommon#",
+    ],
+
+    emojiMouthUncommon: [
+        "D",
+        "3",
+        "o",
+        "O",
+    ],
+
+    emojiUncommon: [
+        "^^",
+        "^_^",
+    ],
 })
+
+grammar.modifiers.capitalize = function (string) {
+    return string.replace(/^./, function (character) {
+        return character.toUpperCase()
+    })
+}
