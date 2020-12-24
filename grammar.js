d98e612a5152bb95b52a433cda901c405fb524ca
helado de brownie <helado.de.brownie@gmail.com>
2020-12-23 23:49:01 -0500

    hee hoo

--- grammar.js
+++ grammar.js
@@ -3,7 +3,10 @@ var grammar = tracery.createGrammar({
 
     compliment: [
         "I appreciate how hard you #work##maybeWorkModifier#",
+        "I like being #yourFriend#",
+        "you are #aJoy#",
         "you have #aNice# #quality#",
+        "your #somethingThatMatters# #matters#",
     ],
 
     work: [
@@ -15,12 +18,71 @@ var grammar = tracery.createGrammar({
     maybeWorkModifier: [
         "",
         "",
-        " #doSomethingModifier#",
+        " #workModifier#",
     ],
 
-    doSomethingModifier: [
-        "even when things are tough",
-        "to make people around you feel good",
+    workModifier: [
+        "during #tough# times",
+        "even when things are #tough#",
+        "to make #people# feel #good#",
+    ],
+
+    tough: [
+        "difficult",
+        "rough",
+        "tough",
+    ],
+
+    people: [
+        "others",
+        "people",
+        "people around you",
+        "those you care about",
+    ],
+
+    good: [
+        "good",
+        "happy",
+        "like they #areGood#",
+        "nice",
+        "valid",
+        "wanted",
+        "welcome#maybeD#",
+    ],
+
+    areGood: [
+        "are valid",
+        "are wanted",
+        "are welcome",
+        "belong",
+    ],
+
+    maybeD: [
+        "",
+        "d",
+    ],
+
+    yourFriend: [
+        "#friend#s with you",
+        "your #friend#",
+    ],
+
+    friend: [
+        "bud",
+        "friend",
+        "pal",
+    ],
+
+    aJoy: [
+        "a joy #toBeAround#",
+        "one of the good things in this world",
+    ],
+
+    toBeAround: [
+        "to be around",
+        "to exist near",
+        "to know",
+        "to live in the same time with",  
     ],
 
     aNice: [
@@ -33,6 +95,26 @@ var grammar = tracery.createGrammar({
     quality: [
         "laugh",
         "smile",
+    ],
+
+    somethingThatMatters: [
+        "capacity for #capacityThatMatters#",
+        "existence",
+        "hard work",
+    ],
+
+    capacityThatMatters: [
+        "friendship",
+        "kindness",
+        "love",
+    ],
+
+    matters: [
+        "is something to be valued",
+        "is valuable for its own sake",
+        "matters",
+        "will all be worth it",
+        "will be worth it in the end",
     ],
 
     emoji: [
@@ -76,9 +158,16 @@ var grammar = tracery.createGrammar({
     ],
 
     emojiUncommon: [
-        "^^",
-        "^_^",
+        "^#_.limit10#^",
         "(:",
+    ],
+
+    _: [
+        "",
+        "_",
+        "_",
+        "_",
+        "_#_#",
     ],
 })
 
@@ -86,4 +175,8 @@ grammar.modifiers.capitalize = function (string) {
     return string.replace(/^./, function (character) {
         return character.toUpperCase()
     })
+}
+
+grammar.modifiers.limit10 = function (string) {
+    return string.substring(0, 10)
 }
